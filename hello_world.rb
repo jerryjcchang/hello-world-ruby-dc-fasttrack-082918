233a80c953e4fbca81db7245a7f5fb9ccf863a01
JC Chang <jerryjcchang@gmail.com>
2018-09-30 01:33:18 +0000

    automatically backed up by learn

--- hello_world.rb
+++ hello_world.rb
@@ -1,9 +1,6 @@
 def hello_world
   name = ["Athena", "JC", "John", "Liming"]
-  puts "Hello World! My name is #{name[0]}!"
-  puts "Hello World! My name is #{name[1]}!"
-  puts "Hello World! My name is #{name[2]}!"
-  puts "Hello World! My name is #{name[3]}!"
+  puts "Hello World! My name is #{name.sample}."
 end
 
 hello_world
